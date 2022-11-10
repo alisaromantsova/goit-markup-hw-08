a9008811c03dbf48db9171fb48a46ba6fdd69ab8
alisaromantsova <alis.romantsova@gmail.com>
2022-11-11 00:29:12 +0200

    test

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,15 @@
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
+    if (!expanded) {
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${window.scrollY}px`;
+    } else {
+      document.body.style.position = 'initial'; 
+      document.body.style.top = ''; 
+    }
+
     mobileMenuRef.classList.toggle("is-open");
+    
   });
 })();
